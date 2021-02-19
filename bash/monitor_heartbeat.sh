03eace8e26a30a76404940374189540fc284821f
John Barton <johnrbarton27@gmail.com>
2021-02-18 19:26:22 -0500

    Add handling for stream computer not being on/monitoring

--- bash/monitor_heartbeat.sh
+++ bash/monitor_heartbeat.sh
@@ -6,11 +6,14 @@ do
     diffInSeconds=$((currTime - lastWriteTime))
     echo $diffInSeconds
 
-    if [[ $diffInSeconds -gt 3 ]] ; then
+    if [[ $diffInSeconds -gt 3600 ]] ; then
+        echo "Stream Computer is not on - waiting 30 seconds before trying again..."
+	sleep 30
+    elif [[ $diffInSeconds -gt 3 ]] ; then
         ~/scripts/go_to_alarm_page.sh
 	echo "COMPUTER IS DEAD!!!!"
 	exit 1
+    else
+        sleep 1
     fi
-
-    sleep 1
 done
