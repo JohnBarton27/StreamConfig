d81495ba2e43fd2c2b13d37654396f44c3c8e92d
John Barton <johnrbarton27@gmail.com>
2021-02-18 20:08:29 -0500

    Fix monitor_heartbeat script to work better as a cronjob

--- bash/monitor_heartbeat.sh
+++ bash/monitor_heartbeat.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+echo "Starting heartbeat monitor..." > /home/pi/monitor_heartbeat.log
 while [ true ]
 do
     currTime=$(date +%s)
-    lastWriteTime=$(cat ~/heartbeat.txt)
+    lastWriteTime=$(cat /home/pi/heartbeat.txt)
     diffInSeconds=$((currTime - lastWriteTime))
-    echo $diffInSeconds
+    echo $diffInSeconds >> /home/pi/monitor_heartbeat.log
 
-    if [[ $diffInSeconds -gt 3600 ]] ; then
-        echo "Stream Computer is not on - waiting 30 seconds before trying again..."
+    if [[ $diffInSeconds -gt 10 ]] ; then
+        echo "Stream Computer is not on - waiting 30 seconds before trying again..." >> /home/pi/monitor_heartbeat.log
 	sleep 30
     elif [[ $diffInSeconds -gt 3 ]] ; then
         ~/scripts/go_to_alarm_page.sh
-	echo "COMPUTER IS DEAD!!!!"
-	exit 1
+	echo "COMPUTER IS DEAD!!!!" >> /home/pi/monitor_heartbeat.log
+	sleep 10
     else
         sleep 1
     fi
